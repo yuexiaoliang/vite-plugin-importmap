a52695e943e621350eb59b816f15537412d50ec6
岳晓亮 <2220124666@qq.com>
2023-02-24 11:00:39 +0800

    reafctor: 重构部分代码

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import { normalizePath, PluginOption } from 'vite'
 import { FileTree, Mark } from './typings'
-import { createFile, getFileTreeCache } from './utils'
+import {cleanUrl, createFile, getFileTreeCache } from './utils'
 
 
 function plugin(mark: Mark): PluginOption {
@@ -28,89 +28,30 @@ function plugin(mark: Mark): PluginOption {
         return res?.id
       }
 
-      const getDir = (p:string): FileTree | undefined => {
-        const pathFragment = p
-          .replace(root, '')
-          .split(path.sep)
-          .filter((e) => e.length > 0)
+      const handleSourceID = _handleSourceID(mark, root, cache)
 
-        let dir = cache?.root
-        let result
-        if (p === root) return dir
+      if (source.includes('node_modules')) return
 
-        pathFragment.forEach((el, index) => {
-          if (!dir) return
+      if (!importer) return
+      if (importer.includes('node_modules')) return
+      if (importer.endsWith('index.html')) return
 
-          const { children } = dir
-          if (!children) return
-          dir = children[el]
-
-          if (index === pathFragment.length - 1) {
-            result = dir
-            return
-          }
-        })
-
-        return result
-      }
-
-      const handleSourceID = (filename: string) => {
-        if (!mark || typeof mark !== 'string') return filename
-
-        const { name, dir, ext, base } = path.parse(filename)
-        const _dir = getDir(dir)
-        if (!_dir) return null
-        const { files, importConfig } = _dir
-
-        // 匹配 Test.v1.vue 命名的文件
-        const formatted = path.format({ dir, name: `${name}.${mark}`, ext })
-        const basename = path.basename(formatted)
-        if (files[basename]) return files[basename]
-
-        // 匹配 Test[v1,v2,v3].vue 命名的文件
-        const find = Object.keys(files).find((file) => {
-          return file.match(new RegExp(`${name}\\[.*${mark}.*\\]${ext}`, 'i'));
-        });
-        if (find) return files[find]
-
-        // 使用 import.config.json 匹配
-        if (importConfig?.[base]) {
-          const fileImportConfig = importConfig[base]
-          const findName = Object.keys(fileImportConfig).find((key) => {
-            const val = fileImportConfig[key]
-
-            if (!Array.isArray(val)) {
-              throw new Error(`${dir}中 import.config.json 格式错误`)
-            }
-
-            return val.includes(mark)
-          })
-          return findName ? files[findName] : null
-        }
-
-        return null
-      }
-
-      if (!importer) return null
-      if (importer.includes('node_modules')) return null
-      if (source.includes('node_modules')) return null
 
       const id = await getResolveId(source)
-      if (!id) return null
-      if (id.includes('node_modules')) return null
-      if (id.includes('?vue')) return null
-      if (id.includes('plugin-vue:export-helper')) return null
-      if (id.includes('vite/preload-helper')) return null
-      if (importer.endsWith('index.html')) return null
+      if (!id) return
+      if (id.includes('node_modules')) return
+
+      const isVirtual = cleanUrl(id).includes('\0')
+      if (isVirtual) return
 
       const newID = handleSourceID(path.normalize(id))
-      if (!newID) return null
+      if (!newID) return
 
       const resultID = await getResolveId(newID)
-      if (!resultID) return null
-      if (!path.isAbsolute(resultID)) return null
-      if (resultID.includes('node_modules')) return null
-      if (!fs.existsSync(resultID)) return null
+      if (!resultID) return
+      if (resultID.includes('node_modules')) return
+      if (!path.isAbsolute(resultID)) return
+      if (!fs.existsSync(resultID)) return
 
       return normalizePath(resultID)
     }
@@ -118,3 +59,80 @@ function plugin(mark: Mark): PluginOption {
 }
 
 export default plugin
+
+function _handleSourceID(mark: string, root: string, cache: any) {
+  const getDir = _getDir(root, cache)
+
+  return (filename: string) => {
+    if (!mark || typeof mark !== 'string')
+      return filename
+
+    const { name, dir, ext, base } = path.parse(filename)
+    const _dir = getDir(dir)
+    if (!_dir)
+      return
+    const { files, importConfig } = _dir
+
+    // 匹配 Test.v1.vue 命名的文件
+    const formatted = path.format({ dir, name: `${name}.${mark}`, ext })
+    const basename = path.basename(formatted)
+    if (files[basename])
+      return files[basename]
+
+    // 匹配 Test[v1,v2,v3].vue 命名的文件
+    const find = Object.keys(files).find((file) => {
+      return file.match(new RegExp(`${name}\\[.*${mark}.*\\]${ext}`, 'i'))
+    })
+    if (find)
+      return files[find]
+
+    // 使用 import.config.json 匹配
+    if (importConfig?.[base]) {
+      const fileImportConfig = importConfig[base]
+      const findName = Object.keys(fileImportConfig).find((key) => {
+        const val = fileImportConfig[key]
+
+        if (!Array.isArray(val)) {
+          throw new Error(`${dir}中 import.config.json 格式错误`)
+        }
+
+        return val.includes(mark)
+      })
+      return findName ? files[findName] : null
+    }
+
+    return
+  }
+}
+
+function _getDir(root: string, cache: any) {
+  return (p: string): FileTree | undefined => {
+    const pathFragment = p
+      .replace(root, '')
+      .split(path.sep)
+      .filter((e) => e.length > 0)
+
+    let dir = cache?.root
+    let result
+    if (p === root)
+      return dir
+
+    pathFragment.forEach((el, index) => {
+      if (!dir)
+        return
+
+      const { children } = dir
+      if (!children)
+        return
+      dir = children[el]
+
+      if (index === pathFragment.length - 1) {
+        result = dir
+        return
+      }
+    })
+
+    return result
+  }
+}
+
--- src/utils.ts
+++ src/utils.ts
@@ -80,3 +80,10 @@ export const createFile = (p:string, data: string) => {
 
   fs.writeFileSync(p, data)
 }
+
+export const queryRE = /\?.*$/s
+
+export const hashRE = /#.*$/s
+
+export const cleanUrl = (url: string): string =>
+  url.replace(hashRE, '').replace(queryRE, '')
